92a7e75c688d309324a617b15443fd3b7f8acd65
m-4verick <theodoreshin99@icloud.com>
2025-01-07 15:49:02 +0900

    feat: 제품 목록 구성 및 페이징 추가

--- src/widgets/Content/Content.jsx
+++ src/widgets/Content/Content.jsx
@@ -1,13 +1,21 @@
-import BestProduct from './BestProduct/BestProduct';
-import OnSaleProduct from './OnSaleProduct/OnSaleProduct';
+import ProductList from './ProductList/ProductList';
+import {useState} from 'react';
 
 export default function Content() {
+    const [order, setOrder] = useState("recent")
+    const [paging, setPaging] = useState(1)
+
+    const handleOrderChange = (e) => setOrder(e.target.value)
+
+    const handlePageChange = (e) => setPaging(e.target.value)
+
+
     return (
         <div className="mt-[96px] max-w-[1200px] mx-auto">
             <div>
                 <p className="text-[#111827] text-[20px] font-bold">베스트 상품</p>
                 <div className="grid grid-cols-4 gap-[24px] h-[378px] mt-[16px]">
-                    <BestProduct />
+                    <ProductList order="favorite" size="4"/>
                 </div>
             </div>
 
@@ -18,25 +26,51 @@ export default function Content() {
                         <input type="text" placeholder="검색할 상품을 입력해주세요"
                                className="bg-[#F3F4F6] rounded-lg pl-[16px] pr-[107px] py-[12px]"/>
                         <button className="bg-[#3692FF] px-[23px] py-[12px] rounded-lg text-white">상품 등록하기</button>
-                        <select className="border px-[20px] py-[12px] rounded-lg">
-                            <option>최신순</option>
-                            <option>좋아요순</option>
+                        <select onChange={handleOrderChange} value={order}
+                                className="border px-[20px] py-[12px] rounded-lg">
+                            <option value="recent">최신순</option>
+                            <option value="favorite">좋아요순</option>
                         </select>
                     </div>
                 </div>
-                <div className="h-[674px] grid grid-cols-5 gap-[24px] mt-[16px]">
-                    <OnSaleProduct />
+                <div className="h-[674px] grid grid-cols-5 gap-[24px] mt-[24px]">
+                    <ProductList size="10" page={paging} order={order}/>
                 </div>
                 <div className="mt-[43px] flex justify-center gap-[4px]">
-                    <button className="w-[40px] h-[40px] rounded-full border font-bold text-[#6B7280]">&lt;</button>
-                    <button className="w-[40px] h-[40px] rounded-full font-bold text-white bg-[#2F80ED]">1</button>
-                    <button className="w-[40px] h-[40px] rounded-full border font-bold text-[#6B7280] ">2</button>
-                    <button className="w-[40px] h-[40px] rounded-full border font-bold text-[#6B7280] ">3</button>
-                    <button className="w-[40px] h-[40px] rounded-full border font-bold text-[#6B7280] ">4</button>
-                    <button className="w-[40px] h-[40px] rounded-full border font-bold text-[#6B7280]">5</button>
-                    <button className="w-[40px] h-[40px] rounded-full border font-bold text-[#6B7280]">&gt;</button>
+                    <button onClick={() => { setPaging( paging - 1)}} className="w-[40px] h-[40px] rounded-full border font-bold">&lt;</button>
+
+                    {(() => {
+                        const buttons = [];
+                        for (let i = 1; i <= 5; i++) {
+                            buttons.push(
+                                <PageClickButton
+                                    key={i}
+                                    isSelected={Number(paging) === Number(i)}
+                                    handlePageChange={handlePageChange}
+                                    value={i}
+                                />
+                            );
+                        }
+                        return buttons;
+                    })()}
+
+
+                    <button onClick={() => { setPaging(paging + 1)}} className="w-[40px] h-[40px] rounded-full border font-bold">&gt;</button>
                 </div>
             </div>
         </div>
+    )
+}
+const PageClickButton = ({handlePageChange, value, isSelected}) => {
+    if (isSelected) {
+        return <button onClick={() => {
+        }} value={value}
+                       className="w-[40px] h-[40px] rounded-full border font-bold bg-[#2F80ED] text-white ">{value}</button>
+    }
+
+    return (
+
+        <button onClick={handlePageChange} value={value}
+                className="w-[40px] h-[40px] rounded-full border font-bold">{value}</button>
     )
 }
--- src/widgets/Content/ProductList/ProductList.jsx
+++ src/widgets/Content/ProductList/ProductList.jsx
@@ -1,26 +1,29 @@
 import axios from 'axios';
 import {useEffect, useState} from 'react';
 
-export default function BestProduct() {
+export default function BestProduct({ page = 1, size, order}) {
     const [list, setList] = useState([])
+    const [ loading, setLoading ] = useState(false)
 
     useEffect(() => {
         async function fetchList() {
             try {
-                const res = await axios.get('https://panda-market-api.vercel.app/products?page=1&pageSize=4&orderBy=favorite')
+                setLoading(true)
+                const res = await axios.get(`https://panda-market-api.vercel.app/products?page=${page}&pageSize=${size}&orderBy=${order}`)
                 setList(res.data.list)
+                setLoading(false)
             } catch (err) {
                 console.log('err: ' + err);
             }
         }
         fetchList()
-    }, [])
+    }, [page, order])
 
     return (
         <>
             {list.map((item) => (
                 <div key={item.id} className="flex flex-col cursor-pointer">
-                    <img src={item.images} className="rounded-lg w-[282px] h-[282px] object-cover"/>
+                    <img src={item.images} className="rounded-lg object-cover aspect-square"/>
                     <p className="mt-[16px] text-[14px] ">{item.name}</p>
                     <p className="mt-[6px] text-[16px] font-bold">{item.price}</p>
                     <p className="mt-[6px] text-[12px]">♡ {item.favoriteCount}</p>
