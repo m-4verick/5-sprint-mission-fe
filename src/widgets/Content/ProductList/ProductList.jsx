ec0dc6b72e776706685aa0c8c6c95114d6f7a41d
m-4verick <theodoreshin99@icloud.com>
2025-01-08 14:47:06 +0900

    feat: placeholder 이미지 처리

--- src/widgets/Content/ProductList/ProductList.jsx
+++ src/widgets/Content/ProductList/ProductList.jsx
@@ -1,8 +1,16 @@
 import axios from "axios";
 import { useState, useEffect } from "react";
+// import imgDefault from '../../../assets/images/placeholder.png'
 
 export default function ProductList({ page, pageSize, orderBy }) {
   const [list, setList] = useState([]);
+  const imgDefault = `https://placehold.co/600x400/png?text=NO+IMAGE`;
+
+  const handleError = (e) => {
+    if (e.target.src === imgDefault) return;
+    e.target.onerror = null;
+    e.target.src = imgDefault;
+  };
 
   useEffect(() => {
     async function fetchData() {
@@ -15,17 +23,19 @@ export default function ProductList({ page, pageSize, orderBy }) {
     fetchData();
   }, [page, orderBy]);
 
+
   return (
     <>
       {list.map((item) => (
         <div key={item.id} className="cursor-pointer">
           <img
-            src={item.images}
-            alt="이미지"
-            className="rounded-xl object-cover aspect-square w-full"
+              src={item.images?.[0] || imgDefault}
+              onError={handleError}
+              alt={item.name || "Product Image"}
+            className="rounded-xl object-cover aspect-square w-full transform transition-transform duration-300 ease-out hover:scale-105"
           />
           <p className="text-[14px] mt-[16px]">{item.name}</p>
-          <p className="text-[16px] font-bold mt-[6px]">{item.price}</p>
+          <p className="text-[16px] font-bold mt-[6px]">{item.price}원</p>
           <p className="text-[12px] mt-[6px]">♡ {item.favoriteCount}</p>
         </div>
       ))}
--- src/widgets/Header/Header.jsx
+++ src/widgets/Header/Header.jsx
@@ -7,7 +7,7 @@ export default function Header() {
     const windowWidth = useWindowWidth();
 
   return (
-    <div className="flex items-center justify-between w-full border-b fixed top-0 bg-white h-[70px] px-[16px] md:px-[24px] xl:px-[200px]">
+    <div className="flex items-center justify-between w-full border-b fixed top-0 bg-white h-[70px] px-[16px] md:px-[24px] xl:px-[200px] z-10">
       <div className="flex items-center text-[#4B5563] font-bold text-[16px] md:text-[18px] gap-[8px] md:gap-[20px] xl:gap-[24px]">
           { windowWidth === "mobile" && <img src={titleOnly} /> }
           { windowWidth === "tablet" && <img src={logoImage} /> }
